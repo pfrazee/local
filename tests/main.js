f6ba555cea40005c87bdcda5cc2533df6a28804e
Paul Frazee <pfrazee@gmail.com>
2012-10-07 21:45:24 -0500

    added environment config interface

--- tests/main.js
+++ tests/main.js
@@ -1,13 +1,9 @@
-Util.logMode('errors', true);
+// == SECTION Environment
+
 Util.logMode('traffic', true);
-Util.logMode('sessions', true);
-//Util.logMode('routing', true);
-//Util.logMode('err_types', true);
 Env.init({
 	getContainerElem:function() { return document.getElementById('linkshell'); },
 	init:function() {
-		var b = Env.makeAgent('otherguy');
-		b.loadProgram('/usr/pfraze/debug.js');
 		var a = Env.makeAgent('inbox', { noclose:true });
 		a.loadProgram('/usr/pfraze/inbox.js', {
 			services:[{ name:'@linkshui', uri:'http://linkshui.com:8600' }]
