282e089d03b361c6aa3cf083ab9ec8a24b37d175
Paul Frazee <pfrazee@gmail.com>
2012-12-27 22:57:51 -0600

    added support for in-document servers via sub-prototyping of App.Server

--- index.js
+++ index.js
@@ -9,12 +9,15 @@ env.onAuthRequest(function(auth, cb) {
 	// :TODO:
 });
 
+// instantiate services
+//env.addServer('localstorage.env', new LocalStorageServer()); :TODO:
+
 // instantiate apps
-env.spawnServer('intro.doc', '/assets/apps/doc/intro.js');
-env.spawnServer('lib.doc', '/assets/apps/doc/lib.js');
-env.spawnServer('request-stream.ui', '/assets/apps/widgets/request-stream.js');
+env.addServer('intro.doc', new App.WorkerServer('/assets/apps/doc/intro.js'));
+env.addServer('lib.doc', new App.WorkerServer('/assets/apps/doc/lib.js'));
+env.addServer('request-stream.ui', new App.WorkerServer('/assets/apps/widgets/request-stream.js'));
 
 // load client regions
-env.spawnClient('#intro').request('httpl://intro.doc');
-env.spawnClient('#lib').request('httpl://lib.doc/linkjs');
-env.spawnClient('#request-stream').request('httpl://request-stream.ui');
+env.addClient('#intro').request('httpl://intro.doc');
+env.addClient('#lib').request('httpl://lib.doc/linkjs');
+env.addClient('#request-stream').request('httpl://request-stream.ui');
