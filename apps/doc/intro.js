b40d55f9adcd629a1c2383a2c5da84e8c73e4bb3
Paul Frazee <pfrazee@gmail.com>
2013-02-03 14:44:51 -0600

    updates to copy and the env client region

--- apps/doc/intro.js
+++ apps/doc/intro.js
@@ -6,8 +6,8 @@ app.onHttpRequest(function(request, response) {
 			'<h1>Local <small>0.2.0 unstable</small></h1>',
 			'<p>Safely run user applications on the page using Web Workers.</p>',
 			'<h4>Introduction</h4>',
-			'<p>Local is an HTTP abstraction over the Web Worker \'postMessage\' API. It allows local servers to run in browser threads, where they host HTML and act as proxies to remote services. Because the servers are unable to access the document\'s namespace, execute inline scripts (due to CSP) or break the message routing policies of the host document, they can be used as safe hosts for untrusted software.',
-			'<blockquote><small>Click the script icons on the top right of an app to view and modify its source.</small></blockquote>'
+			'<p>Local is an HTTP abstraction over the Web Worker \'postMessage\' API. It allows local servers to run in browser threads, where they host HTML and act as proxies to remote services. Because the servers are unable to access the document\'s namespace, execute inline scripts (due to CSP) or break the message routing policies of the host document, they can be used as containers for untrusted software.',
+			'<blockquote><small>Click the script icons on the top right of this page\'s apps to view and modify their source.</small></blockquote>'
 		].join(''));
 	}).error(response);
 });
