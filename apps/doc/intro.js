8c347a8ba8e8a65940cc49fec31a59494359858c
Paul Frazee <pfrazee@gmail.com>
2013-02-27 00:19:17 -0600

    version bumps

--- apps/doc/intro.js
+++ apps/doc/intro.js
@@ -3,7 +3,7 @@ importScripts('linkjs-ext/router.js');
 app.onHttpRequest(function(request, response) {
 	Link.router(request).mpa('get', '/', /html/, function() {
 		Link.responder(response).ok('html').end([
-			'<h1>Local <small>0.2.0</small></h1>',
+			'<h1>Local <small>0.2.1</small></h1>',
 			'<p>Safely run user applications on the page using Web Workers.</p>',
 			'<h4>Introduction</h4>',
 			'<p>Local is an HTTP abstraction over the Web Worker \'postMessage\' API. It allows local servers to run in browser threads, where they host HTML and act as proxies to remote services. Because the servers are unable to access the document\'s namespace, execute inline scripts (due to CSP) or break the message routing policies of the host document, they can be used as containers for untrusted software.',
