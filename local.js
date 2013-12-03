233edaafda671cd5af8b29c4788ee12e7924a729
Paul Frazee <pfrazee@gmail.com>
2013-12-03 11:31:22 -0600

    removed the try/catch block in nexttick

--- local.js
+++ local.js
@@ -756,11 +756,26 @@ local.util.deepClone = function(obj) {
 	return JSON.parse(JSON.stringify(obj));
 };
 
-// https://github.com/timoxley/next-tick
+
 // fallback for other environments / postMessage behaves badly on IE8
 if (typeof window == 'undefined' || window.ActiveXObject || !window.postMessage) {
 	local.util.nextTick = function(fn) { setTimeout(fn, 0); };
 } else {
+	var nextTickIndex = 0, nextTickFns = {};
+	local.util.nextTick = function(fn) {
+		window.postMessage('nextTick'+nextTickIndex, '*');
+		nextTickFns['nextTick'+nextTickIndex] = fn;
+		nextTickIndex++;
+	};
+	window.addEventListener('message', function(evt){
+		var fn = nextTickFns[evt.data];
+		delete nextTickFns[evt.data];
+		fn();
+	}, true);
+
+	// The following is the original version by // https://github.com/timoxley/next-tick
+	// It was replaced by the above to avoid the try/catch block
+	/*
 	var nextTickQueue = [];
 	local.util.nextTick = function(fn) {
 		if (!nextTickQueue.length) window.postMessage('nextTick', '*');
@@ -779,6 +794,7 @@ if (typeof window == 'undefined' || window.ActiveXObject || !window.postMessage)
 		}
 		nextTickQueue.length = 0;
 	}, true);
+	*/
 }})();// Local HTTP
 // ==========
 // pfraze 2013
